3418d7f2b9515b930116176430aa266e5baa2b75
Ihor <igorok.vin@gmail.com>
2025-04-23 21:52:21 -0600

    add WeatherNetworkService project

---
+++ WeatherNetworkApiService/src/main/java/com/skyapi/weathernetworkapi/WeatherNetworkApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.skyapi.weathernetworkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherNetworkApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherNetworkApiServiceApplication.class, args);
+	}
+
+}
---
+++ WeatherNetworkApiService/src/test/java/com/skyapi/weathernetworkapi/WeatherNetworkApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skyapi.weathernetworkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherNetworkApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
