17f9416e9d8f49d9d3cada027ff57b451e25dc62
Ihor <igorok.vin@gmail.com>
2025-04-23 21:41:33 -0600

    add WeatherNetworkCommon project

---
+++ src/main/java/com/skyapi/weathernetworkapiproject/WeatherNetworkApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.skyapi.weathernetworkapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherNetworkApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherNetworkApiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skyapi/weathernetworkapiproject/WeatherNetworkApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skyapi.weathernetworkapiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherNetworkApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
